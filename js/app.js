42cbd9c1e53d8f18ba1430c1c53c60c68b56593b
tingru <guotingru@outlook.com>
2014-11-16 01:36:07 -0800

    change 2

--- js/app.js
+++ js/app.js
@@ -3,8 +3,57 @@
     app.js, main Angular application script
     define your module and controllers here
 */
+document.getElementById("form-comment").addEventListener("submit", onSubmit);
+
+function onSubmit(evt) {
+    console.log("Inside onSubmit!");
+    var valid = true;
+    try {
+        valid = validateForm(this);
+    } catch (exception) {
+        valid = false;
+        console.log(exception);
+        alert(exception);
+    }
+
+    // stop form submission if validation failed
+    if (!valid && evt.preventDefault) {
+        evt.preventDefault();
+    }
+
+    evt.returnValue = valid;
+    return valid;
+}
+
+function validateForm(commentForm) {
+    var isValid = true;
+
+    var nameField = commentForm.elements["comment-name"];
+    var titleField = commentForm.elements["comment-title"];
+    var bodyField = commentForm.elements["comment-body"];
+
+    // validates all the required fields
+    isValid &= validateString(nameField);
+    isValid &= validateString(titleField);
+    isValid &= validateString(bodyField);
+
+    return isValid;
+}
+
+// validate common string field
+function validateString(field) {
+    if (0 == field.value.trim().length) {
+        field.className = "form-control invalid-field";
+        return false;
+    } else {
+        field.className = "form-control";
+        return true;
+    }
+}
+
 
 var commentsUrl = 'https://api.parse.com/1/classes/ajaxcomments';
+var errorMessage = document.getElementById("comment-message");
 
 angular.module('CommentsApp', ['ui.bootstrap'])
     .config(function ($httpProvider) {
@@ -27,16 +76,37 @@ angular.module('CommentsApp', ['ui.bootstrap'])
         $scope.newComment = {
             rating: null,
             name: '',
-            title: '',            
+            title: '',
             body: ''
         };
 
         $scope.addComment = function () {
             $scope.updating = true;
+            if ($scope.newComment.rating == null || $scope.newComment.name == '' || $scope.newComment.title == '' || $scope.newComment.body == '') {
+
+                if ($scope.newComment.rating == null) {
+                    errorMessage.style.display = "block";
+                    errorMessage.innerHTML = "Please rate 1 to 5.";
+                } else {
+                    errorMessage.style.display = "none";
+                }
+                $scope.updating = false;
+                return;
+            }
             $http.post(commentsUrl, $scope.newComment)
                 .success(function (responseData) {
                     $scope.newComment.objectId = responseData.objectId;
                     $scope.comments.push($scope.newComment);
+                    $scope.newComment = {
+                        rating: null,
+                        name: '',
+                        title: '',
+                        body: ''
+                    };
+                })
+                .error(function (err) {
+                    errorMessage.style.display = "block";
+                    errorMessage.innerHTML = err.error;
                 })
                 .finally(function () {
                     $scope.updating = false;
